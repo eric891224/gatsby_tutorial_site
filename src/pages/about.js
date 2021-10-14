13f50464539426d11db669ed62458e26be862e6a
eric891224 <s96006730@gmail.com>
2021-10-15 00:21:09 +0800

    finish tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const AboutPage = (props) => {
+    return (
+        <Layout pageTitle="About me">
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage;
